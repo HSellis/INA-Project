2e2db67ea2466d0169b83768ed16daeacce2b3e1
Henri Sellis <henri.sellis@gmail.com>
2025-05-17 13:56:19 +0200

    sector and sub-industry values from wiki to nodes

--- stock_graph_creation.py
+++ stock_graph_creation.py
@@ -1,34 +1,30 @@
-import yfinance
 import pandas as pd
 import networkx as nx
 
 
-def get_sp500_stocks(start_date, end_date):
+def get_sp500_stocks_info():
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     tables = pd.read_html(url)
     table = tables[0]
-    stocks = list(table["Symbol"])
-    dataset = yfinance.download(stocks, start=start_date, end=end_date)
-    df_filtered = dataset["Close"] / dataset["Open"]
-    return df_filtered
+    stocks_info = {}
+    for _, row in table.iterrows():
+        stocks_info[row["Symbol"]] = {
+            "sector": row["GICS Sector"],
+            "sub-industry": row["GICS Sub-Industry"]
+        }
+    # COIN was changed to DFS, so downloading this puts non-matching name in the dataframe, which creates errors...
+    del stocks_info["COIN"]
+    return stocks_info
 
-def correlation_to_graph(corr_matrix: pd.DataFrame, threshold: float = 0.7) -> nx.Graph:
-    """
-    Create a graph from a correlation matrix.
-    
-    Parameters:
-    - corr_matrix: A pandas DataFrame representing the correlation matrix (stocks × stocks).
-    - threshold: Minimum absolute correlation value to include an edge.
-
-    Returns:
-    - G: A networkx.Graph object with nodes and weighted edges.
-    """
+def correlation_to_graph(corr_matrix: pd.DataFrame, stocks_info: dict, threshold: float = 0.7) -> nx.Graph:
     G = nx.Graph()
     G.name = "correlations"
     
     # Add nodes (stock tickers)
     for i, stock in enumerate(corr_matrix.columns):
-        G.add_node(i, label=stock)
+        stock_info = stocks_info[stock]
+        print(stock)
+        G.add_node(i, label=stock, sector=stock_info["sector"], industry=stock_info["sub-industry"])
 
     # Add edges for correlations above threshold
     for i in range(len(corr_matrix.columns)):
@@ -40,7 +36,7 @@ def correlation_to_graph(corr_matrix: pd.DataFrame, threshold: float = 0.7) -> n
 
     return G
 
-def correlation_to_pos_neg_graphs(corr_matrix: pd.DataFrame, threshold: float = 0.7):
+def correlation_to_pos_neg_graphs(corr_matrix: pd.DataFrame, stocks_info: dict, threshold: float = 0.7):
     """
     one graph with strong positive correlation edges and another with strong negative correlation edges
     """
@@ -50,8 +46,9 @@ def correlation_to_pos_neg_graphs(corr_matrix: pd.DataFrame, threshold: float = 
     G_neg.name = "neg_correlations"
 
     for i, stock in enumerate(corr_matrix.columns):
-        G_pos.add_node(i, label=stock)
-        G_neg.add_node(i, label=stock)
+        stock_info = stocks_info[stock]
+        G_pos.add_node(i, label=stock, sector=stock_info["sector"], industry=stock_info["sub-industry"])
+        G_neg.add_node(i, label=stock, sector=stock_info["sector"], industry=stock_info["sub-industry"])
 
     for i in range(len(corr_matrix.columns)):
         for j in range(i + 1, len(corr_matrix.columns)):
